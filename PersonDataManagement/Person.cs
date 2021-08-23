e032dc3c366b7f7b074b788493a560b5ae8a948d
saurabhs <saurabhpshirsath@gmail.com>
2021-08-23 21:34:12 +0530

    [saurabh]Add:List of person and add value to list

---
+++ PersonDataManagement/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagement
+{
+    class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person()
+        {
+            SSN = "";
+            Name = "";
+            Address = "";
+            Age = 0;
+        }
+        public Person(string SSN, string Name, string Address, int Age)
+        {
+            this.SSN = SSN;
+            this.Name = Name;
+            this.Address = Address;
+            this.Age = Age;
+        }
+    }
+}
+
---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Person Data Management!");
+            List<Person> personList = new List<Person>();
+            AddRecords(personList);
+
+        }
+        private static void AddRecords(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person("203456876", "AJ", "12 Main Street, Washington,DC", 15));
+            listPersonInCity.Add(new Person("203456877", "AK", "13 Main Ct, Washington,DC", 25));
+            listPersonInCity.Add(new Person("203456878", "Elan", "14 Main Street, Washington,DC", 35));
+            listPersonInCity.Add(new Person("203456879", "Smith", "12 Main Street, Washington,DC", 45));
+            listPersonInCity.Add(new Person("203456879", "Smith", "12 Main Street, Washington,DC", 45));
+            listPersonInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Washington,DC", 55));
+            listPersonInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 54));
+            listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
+            listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 80));
+            listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
+        }
+    }
+}
